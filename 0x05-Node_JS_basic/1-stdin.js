8f35481f29e1ea063d8c191e5404bc441e96c578
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2023-08-25 11:46:09 +0100

    Fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,3 @@
-/**
- * Using Process stdin
- */
-
-// process.stdin.setEncoding("utf8");
-
-// console.log("Welcome to Holberton School, what is your name?");
-
-// process.stdin.on("data", (data) => {
-//   console.log(`Your name is: ${data}`);
-//   process.stdin.pause();
-// });
-
-// console.log("This important software is now closing");
-
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on("readable", () => {
